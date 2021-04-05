33f03d36a54f2e65561b4d826c3b8914800b7a24
Benjamin Tan <benjamin@dev.ofcr.se>
2021-04-05 22:25:11 +0800

    completion: Add `insertText` field for enum completions
    Another bug with the older version of lsp-mode on Emacs prevents the
    label from being inserted when there is an empty `insertText`. This
    commit adds an `insertText` property to enum completions for consistency
    with the rest of the completion items.

--- src/analysis.zig
+++ src/analysis.zig
@@ -2672,6 +2672,8 @@ fn makeScopeInternal(
                     try enum_completions.put(allocator, .{
                         .label = name,
                         .kind = .Constant,
+                        .insertText = name,
+                        .insertTextFormat = .PlainText,
                         .documentation = if (try getDocComments(allocator, tree, decl, .Markdown)) |docs| .{
                             .kind = .Markdown,
                             .value = docs,
