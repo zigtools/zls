542f49087e79cdd180ae483743b2dade3c7b314c
LeRoyce Pearson <contact@leroycepearson.dev>
2022-07-18 14:35:00 -0600

    Add known-folders as a flake input
    This allows the flake to work without passing in `?submodules=1`, which
    makes it easy to include in other flakes.
    
    This commit also makes it possible to override the path to the
    `known-folders` package like so:
    
    ```
    zig build -Dknown-folders=/path/to/known-folders.zig
    ```
    
    This allows `flake.nix` to pass in the nix store path.

--- flake.nix
+++ flake.nix
@@ -12,21 +12,43 @@
   outputs = {self, nixpkgs, zig-overlay, gitignore }:
     let
       inherit (gitignore.lib) gitignoreSource;
-      zls-derivation-fn = (system: nixpkgs.legacyPackages.${system}.stdenvNoCC.mkDerivation {
+
+      known-folders-fetch = (system: nixpkgs.legacyPackages.${system}.stdenvNoCC.mkDerivation {
+        name = "known-folders";
+        src = nixpkgs.legacyPackages.${system}.fetchFromGitHub {
+          owner = "ziglibs";
+          repo = "known-folders";
+          rev = "9db1b99219c767d5e24994b1525273fe4031e464";
+          sha256 = "sha256-eqaZxIax8C75L2UwDbVKSUZ7iThm/iWblfoaTfPyHLM=";
+        };
+        dontConfigure = true;
+        dontInstall = true;
+        buildPhase = ''
+          mkdir -p $out
+          cp known-folders.zig $out
+        '';
+      });
+
+      zls-derivation-fn = (system: nixpkgs.legacyPackages.${system}.stdenvNoCC.mkDerivation
+        (let known-folders = known-folders-fetch system;
+        in {
         name = "zls";
         version = "master";
         src = gitignoreSource ./.;
         nativeBuildInputs = [
           zig-overlay.packages.${system}.master.latest
         ];
+        buildInputs = [
+          known-folders
+        ];
         dontConfigure = true;
         dontInstall = true;
         buildPhase = ''
           mkdir -p $out
-          zig build install -Drelease-safe=true -Ddata_version=master --prefix $out
+          zig build install -Drelease-safe=true -Ddata_version=master -Dknown-folders=${known-folders}/known-folders.zig --prefix $out
         '';
         XDG_CACHE_HOME = ".cache";
-      });
+      }));
     in {
       packages = rec {
         x86_64-linux.zls = zls-derivation-fn "x86_64-linux";
