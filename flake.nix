6420296a822369baf34abf8ad44d32e12bba1622
Sage Hane <sage@sagehane.com>
2022-11-03 18:44:01 +0000

    Nix: Pass `-Dcpu=baseline` to `zig build` (#739)
    * update flake.lock
    
    * Nix: Pass `-Dcpu=baseline` to `zig build`

--- flake.nix
+++ flake.nix
@@ -13,16 +13,18 @@
     known-folders.url = "github:ziglibs/known-folders";
     known-folders.flake = false;
   };
-  
-  outputs = {self, nixpkgs, zig-overlay, gitignore, flake-utils, known-folders }:
+
+  outputs = { self, nixpkgs, zig-overlay, gitignore, flake-utils, known-folders }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       inherit (gitignore.lib) gitignoreSource;
-    in flake-utils.lib.eachSystem systems (system:
+    in
+    flake-utils.lib.eachSystem systems (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         zig = zig-overlay.packages.${system}.master;
-      in rec {
+      in
+      rec {
         packages.default = packages.zls;
         packages.zls = pkgs.stdenvNoCC.mkDerivation {
           name = "zls";
@@ -33,10 +35,10 @@
           dontInstall = true;
           buildPhase = ''
             mkdir -p $out
-            zig build install -Drelease-safe=true -Ddata_version=master -Dknown-folders=${known-folders}/known-folders.zig --prefix $out
+            zig build install -Dcpu=baseline -Drelease-safe=true -Ddata_version=master -Dknown-folders=${known-folders}/known-folders.zig --prefix $out
           '';
           XDG_CACHE_HOME = ".cache";
         };
       }
-  );
+    );
 }
