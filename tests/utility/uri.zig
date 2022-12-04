8f2cae7bb805c5db1174c3f1f41863b6badf6cd1
Auguste Rame <19855629+SuperAuguste@users.noreply.github.com>
2022-12-03 22:40:31 -0500

    Fix test failures, revert errdefer->defer

--- tests/utility/uri.zig
+++ tests/utility/uri.zig
@@ -5,6 +5,22 @@ const URI = zls.URI;
 
 const allocator = std.testing.allocator;
 
+test "uri - parse" {
+    const parseWin = try URI.parse(allocator, "file:///c%3A/main.zig");
+    defer allocator.free(parseWin);
+    try std.testing.expectEqualStrings("c:\\main.zig", parseWin);
+
+    const parseWin2 = try URI.parse(allocator, "file:///c%3A/main%2B.zig");
+    defer allocator.free(parseWin2);
+    try std.testing.expectEqualStrings("c:\\main+.zig", parseWin2);
+}
+
+test "uri - fromPath" {
+    const fromPathWin = try URI.fromPath(allocator, "c:\\main.zig");
+    defer allocator.free(fromPathWin);
+    try std.testing.expectEqualStrings("file:///c%3A/main.zig", fromPathWin);
+}
+
 test "uri - pathRelative" {
     const join1 = try URI.pathRelative(allocator, "file://project/zig", "/src/main+.zig");
     defer allocator.free(join1);
@@ -13,16 +29,4 @@ test "uri - pathRelative" {
     const join2 = try URI.pathRelative(allocator, "file://project/zig/wow", "../]src]/]main.zig");
     defer allocator.free(join2);
     try std.testing.expectEqualStrings("file://project/zig/%5Dsrc%5D/%5Dmain.zig", join2);
-
-    const fromPathWin = try URI.fromPath(allocator, "C:\\main.zig");
-    defer allocator.free(fromPathWin);
-    try std.testing.expectEqualStrings("file:///c%3A/main.zig", fromPathWin);
-
-    const parseWin = try URI.parse(allocator, "file:///c%3A/main.zig");
-    defer allocator.free(parseWin);
-    try std.testing.expectEqualStrings("c:\\main.zig", parseWin);
-
-    const parseWin2 = try URI.parse(allocator, "file:///c%3A/main%2B.zig");
-    defer allocator.free(parseWin2);
-    try std.testing.expectEqualStrings("c:\\main+.zig", parseWin2);
 }
