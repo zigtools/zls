997bafd923d38381ec3ffbe7b1399bbc55b5a5de
Techatrix <19954306+Techatrix@users.noreply.github.com>
2023-01-06 02:39:58 +0100

    run zig fmt

--- build.zig
+++ build.zig
@@ -52,7 +52,7 @@ pub fn build(b: *std.build.Builder) !void {
         "enable_tracy_callstack",
         b.option(bool, "enable_tracy_callstack", "Enable callstack graphs.") orelse false,
     );
-    
+
     exe_options.addOption(
         bool,
         "enable_failing_allocator",
