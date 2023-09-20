b6397744502fbe787e96ef77aa285b8391c99e7a
Lee Cannon <leecannon@leecannon.xyz>
2023-09-20 18:08:09 +0100

    update known-folders (#1450)
    * update known-folders
    
    * update known-folders in flake.nix
    
    ---------
    
    Co-authored-by: Techatrix <19954306+Techatrix@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       diffz.url = "https://github.com/ziglibs/diffz/archive/90353d401c59e2ca5ed0abe5444c29ad3d7489aa.tar.gz";
       diffz.flake = false;
 
-      known_folders.url = "https://github.com/ziglibs/known-folders/archive/fa75e1bc672952efa0cf06160bbd942b47f6d59b.tar.gz";
+      known_folders.url = "https://github.com/ziglibs/known-folders/archive/a564f582122326328dad6b59209d070d57c4e6ae.tar.gz";
       known_folders.flake = false;
     };
 
