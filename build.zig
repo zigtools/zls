51170dbc4008ed0255ed3fa477a6977df100af18
Techatrix <19954306+Techatrix@users.noreply.github.com>
2023-10-14 15:37:19 +0200

    set minimum build version to `0.12.0-dev.899+027aabf49`

--- build.zig
+++ build.zig
@@ -4,8 +4,8 @@ const builtin = @import("builtin");
 const zls_version = std.SemanticVersion{ .major = 0, .minor = 12, .patch = 0 };
 
 /// document the latest breaking change that caused a change to the string below:
-/// Package.Fetch: wrap operations involving tmp_directory in a block
-const min_zig_string = "0.12.0-dev.890+8c6b0271c";
+/// drop for loop syntax upgrade mechanisms
+const min_zig_string = "0.12.0-dev.899+027aabf49";
 
 pub fn build(b: *std.build.Builder) !void {
     comptime {
