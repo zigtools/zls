e8b3be9d3e56b7891ce4b1f6f730201651e516f3
Magnus Holm <judofyr@gmail.com>
2024-02-17 16:01:18 +0100

    Nix: Make it possible to override -Doptimize

--- zls.nix
+++ zls.nix
@@ -1,4 +1,4 @@
-{ callPackage, stdenvNoCC, gitignoreSource, zig, langref }:
+{ callPackage, stdenvNoCC, gitignoreSource, zig, langref, zigOptimize ? "ReleaseSafe" }:
 stdenvNoCC.mkDerivation {
   name = "zls";
   version = "master";
@@ -8,10 +8,11 @@ stdenvNoCC.mkDerivation {
   dontInstall = true;
   doCheck = true;
   langref = langref;
+  zigOptimize = zigOptimize;
   buildPhase = ''
     mkdir -p .cache
     ln -s ${callPackage ./deps.nix { }} .cache/p
-    zig build install --cache-dir $(pwd)/zig-cache --global-cache-dir $(pwd)/.cache -Dversion_data_path=$langref -Dcpu=baseline -Doptimize=ReleaseSafe --prefix $out
+    zig build install --cache-dir $(pwd)/zig-cache --global-cache-dir $(pwd)/.cache -Dversion_data_path=$langref -Dcpu=baseline -Doptimize=$zigOptimize --prefix $out
   '';
   checkPhase = ''
     zig build test --cache-dir $(pwd)/zig-cache --global-cache-dir $(pwd)/.cache -Dversion_data_path=$langref -Dcpu=baseline
