e41af363fdc53b4b10fdfeb05143effc8d27cfd3
Techatrix <techatrix@mailbox.org>
2024-11-26 12:25:10 +0100

    change BuildConfig.zig to shared.zig

---
+++ src/build_runner/shared.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub const BuildConfig = struct {
+    deps_build_roots: []DepsBuildRoots,
+    packages: []Package,
+    include_dirs: []const []const u8,
+    top_level_steps: []const []const u8,
+    available_options: std.json.ArrayHashMap(AvailableOption),
+
+    pub const DepsBuildRoots = Package;
+    pub const Package = struct {
+        name: []const u8,
+        path: []const u8,
+    };
+    pub const AvailableOption = std.meta.FieldType(std.meta.FieldType(std.Build, .available_options_map).KV, .value);
+};
