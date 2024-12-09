0927d16c3ee0c4fc25d30ec702e8c4acce8fa1e6
Techatrix <techatrix@mailbox.org>
2024-12-10 00:12:15 +0100

    set minimum runtime version to `0.14.0-dev.310+9d38e82b5`

--- build.zig
+++ build.zig
@@ -18,14 +18,14 @@ const zls_version = std.SemanticVersion{ .major = 0, .minor = 14, .patch = 0, .p
 const minimum_build_zig_version = "0.14.0-dev.2472+cc82620b2";
 
 /// Specify the minimum Zig version that is required to run ZLS:
-/// Build Runner: Implement File System Watching for kqueue
+/// make zig compiler processes live across rebuilds
 ///
 /// Examples of reasons that would cause the minimum runtime version to be bumped are:
 ///   - breaking change to the Zig Syntax
 ///   - breaking change to AstGen (i.e `zig ast-check`)
 ///
 /// A breaking change to the Zig Build System should be handled by updating ZLS's build runner (see src\build_runner)
-const minimum_runtime_zig_version = "0.14.0-dev.2046+b8795b4d0";
+const minimum_runtime_zig_version = "0.14.0-dev.310+9d38e82b5";
 
 const release_targets = [_]std.Target.Query{
     .{ .cpu_arch = .x86_64, .os_tag = .windows },
