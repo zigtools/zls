ed90d2ab815d09e60ba73d7348d56bad910d1ea3
Techatrix <techatrix@mailbox.org>
2025-02-12 01:34:33 +0100

    update `FailingAllocator` to new allocator interface

--- src/debug.zig
+++ src/debug.zig
@@ -97,6 +97,7 @@ pub const FailingAllocator = struct {
             .vtable = &.{
                 .alloc = alloc,
                 .resize = resize,
+                .remap = remap,
                 .free = free,
             },
         };
@@ -105,35 +106,44 @@ pub const FailingAllocator = struct {
     fn alloc(
         ctx: *anyopaque,
         len: usize,
-        log2_ptr_align: u8,
-        return_address: usize,
+        alignment: std.mem.Alignment,
+        ret_addr: usize,
     ) ?[*]u8 {
         const self: *FailingAllocator = @ptrCast(@alignCast(ctx));
         if (shouldFail(self)) return null;
-        return self.internal_allocator.rawAlloc(len, log2_ptr_align, return_address);
+        return self.internal_allocator.rawAlloc(len, alignment, ret_addr);
     }
 
     fn resize(
         ctx: *anyopaque,
-        old_mem: []u8,
-        log2_old_align: u8,
+        memory: []u8,
+        alignment: std.mem.Alignment,
         new_len: usize,
-        ra: usize,
+        ret_addr: usize,
     ) bool {
         const self: *FailingAllocator = @ptrCast(@alignCast(ctx));
-        if (!self.internal_allocator.rawResize(old_mem, log2_old_align, new_len, ra))
-            return false;
-        return true;
+        return self.internal_allocator.rawResize(memory, alignment, new_len, ret_addr);
+    }
+
+    fn remap(
+        ctx: *anyopaque,
+        memory: []u8,
+        alignment: std.mem.Alignment,
+        new_len: usize,
+        ret_addr: usize,
+    ) ?[*]u8 {
+        const self: *FailingAllocator = @ptrCast(@alignCast(ctx));
+        return self.internal_allocator.rawRemap(memory, alignment, new_len, ret_addr);
     }
 
     fn free(
         ctx: *anyopaque,
-        old_mem: []u8,
-        log2_old_align: u8,
-        ra: usize,
+        memory: []u8,
+        alignment: std.mem.Alignment,
+        ret_addr: usize,
     ) void {
         const self: *FailingAllocator = @ptrCast(@alignCast(ctx));
-        self.internal_allocator.rawFree(old_mem, log2_old_align, ra);
+        self.internal_allocator.rawFree(memory, alignment, ret_addr);
     }
 
     fn shouldFail(self: *FailingAllocator) bool {
