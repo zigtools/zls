ea13de83a6b2e50fe811b7cc438fa04df9e48f17
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2025-02-14 08:41:58 -0800

    `typeValFromIP` -> `fromIP`

--- src/features/completions.zig
+++ src/features/completions.zig
@@ -257,7 +257,7 @@ fn declToCompletion(builder: *Builder, decl_handle: Analyser.DeclWithHandle, opt
                     kind = .Struct;
                 } else if (decl_handle.decl == .function_parameter and ty.isMetaType()) {
                     kind = .TypeParameter;
-                } else if (ty.isEnumLiteral(builder.analyser)) {
+                } else if (ty.isEnumLiteral()) {
                     kind = .EnumMember;
                 } else if (ty.data == .compile_error) {
                     is_deprecated = true;
@@ -1645,7 +1645,7 @@ fn collectEnumLiteralContainerNodes(
         el_dot_context,
     );
     for (containers) |container| {
-        const container_instance = try container.instanceTypeVal(analyser) orelse container;
+        const container_instance = container.instanceTypeVal(analyser) orelse container;
         const member_decl = try container_instance.lookupSymbol(analyser, alleged_field_name) orelse continue;
         var member_type = try member_decl.resolveType(analyser) orelse continue;
         // Unwrap `x{ .fld_w_opt_type =`
