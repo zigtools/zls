21c1b67a187f02b8f3a96eee172632e78995f972
Techatrix <techatrix@mailbox.org>
2025-03-06 23:58:52 +0100

    document the correct path to itself in build runner

--- src/build_runner/0.14.0.zig
+++ src/build_runner/0.14.0.zig
@@ -6,11 +6,11 @@
 //! Handling multiple Zig versions can be achieved by branching on the `builtin.zig_version` at comptime.
 //!
 //! You can test out the build runner on ZLS's `build.zig` with the following command:
-//! `zig build --build-runner src/build_runner/master.zig`
+//! `zig build --build-runner src/build_runner/0.14.0.zig`
 //!
 //! You can also test the build runner on any other `build.zig` with the following command:
-//! `zig build --build-file /path/to/build.zig --build-runner /path/to/zls/src/build_runner/master.zig`
-//! `zig build --build-runner /path/to/zls/src/build_runner/master.zig` (if the cwd contains build.zig)
+//! `zig build --build-file /path/to/build.zig --build-runner /path/to/zls/src/build_runner/0.14.0.zig`
+//! `zig build --build-runner /path/to/zls/src/build_runner/0.14.0.zig` (if the cwd contains build.zig)
 //!
 
 const root = @import("@build");
