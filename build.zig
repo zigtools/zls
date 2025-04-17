5e8d9cf1b4d724a596a3ac02adc49dcc7033a904
Techatrix <techatrix@mailbox.org>
2025-04-17 21:57:06 +0200

    set minimum build version to `0.15.0-dev.346+f32a5d349`

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ const builtin = @import("builtin");
 const zls_version: std.SemanticVersion = .{ .major = 0, .minor = 15, .patch = 0, .pre = "dev" };
 
 /// Specify the minimum Zig version that is required to compile and test ZLS:
-/// std.Build: implement addEmbedPath for adding C #embed search directories
+/// std: eradicate u29 and embrace std.mem.Alignment
 ///
 /// If you do not use Nix, a ZLS maintainer can take care of this.
 /// Whenever this version is increased, run the following command:
@@ -15,7 +15,7 @@ const zls_version: std.SemanticVersion = .{ .major = 0, .minor = 15, .patch = 0,
 /// ```
 ///
 /// Also update the `minimum_zig_version` in `build.zig.zon`.
-const minimum_build_zig_version = "0.15.0-dev.141+b5a526054";
+const minimum_build_zig_version = "0.15.0-dev.346+f32a5d349";
 
 /// Specify the minimum Zig version that is required to run ZLS:
 /// Release 0.14.0
