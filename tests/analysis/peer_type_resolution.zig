3be6bf9203fb0115a2f8894206a401dde994bbdf
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2025-04-18 08:02:25 -0700

    Avoid using undefined in PTR analysis tests

--- tests/analysis/peer_type_resolution.zig
+++ tests/analysis/peer_type_resolution.zig
@@ -2,45 +2,50 @@ const S = struct {
     int: i64,
     float: f32,
 };
+const s: S = .{
+    .int = 0,
+    .float = 1.2,
+};
 
 pub fn main() void {
-    var runtime_bool: bool = undefined;
+    var runtime_bool: bool = true;
 
-    const widened_int_0 = if (runtime_bool) @as(i8, undefined) else @as(i16, undefined);
+    const widened_int_0 = if (runtime_bool) @as(i8, 0) else @as(i16, 0);
     _ = widened_int_0;
     //  ^^^^^^^^^^^^^ (i16)()
 
-    const widened_int_1 = if (runtime_bool) @as(i16, undefined) else @as(i8, undefined);
+    const widened_int_1 = if (runtime_bool) @as(i16, 0) else @as(i8, 0);
     _ = widened_int_1;
     //  ^^^^^^^^^^^^^ (i16)()
 
-    const optional_0 = if (runtime_bool) @as(S, undefined) else @as(?S, undefined);
+    const optional_0 = if (runtime_bool) s else @as(?S, s);
     _ = optional_0;
     //  ^^^^^^^^^^ (?S)()
 
-    const optional_1 = if (runtime_bool) @as(?S, undefined) else @as(S, undefined);
+    const optional_1 = if (runtime_bool) @as(?S, s) else s;
     _ = optional_1;
     //  ^^^^^^^^^^ (?S)()
 
-    const optional_2 = if (runtime_bool) null else @as(S, undefined);
+    const optional_2 = if (runtime_bool) null else s;
     _ = optional_2;
     //  ^^^^^^^^^^ (?S)()
 
-    const optional_3 = if (runtime_bool) @as(S, undefined) else null;
+    const optional_3 = if (runtime_bool) s else null;
     _ = optional_3;
     //  ^^^^^^^^^^ (?S)()
 
-    const optional_4 = if (runtime_bool) null else @as(?S, undefined);
+    const optional_4 = if (runtime_bool) null else @as(?S, s);
     _ = optional_4;
     //  ^^^^^^^^^^ (?S)()
 
-    const optional_5 = if (runtime_bool) @as(?S, undefined) else null;
+    const optional_5 = if (runtime_bool) @as(?S, s) else null;
     _ = optional_5;
     //  ^^^^^^^^^^ (?S)()
 
-    const comptime_int_and_void = if (runtime_bool) 0 else {};
-    _ = comptime_int_and_void;
-    //  ^^^^^^^^^^^^^^^^^^^^^ (either type)()
-
-    runtime_bool = undefined;
+    _ = &runtime_bool;
 }
+
+const comptime_bool: bool = true;
+
+const comptime_int_and_void = if (comptime_bool) 0 else {};
+//    ^^^^^^^^^^^^^^^^^^^^^ (either type)()
