9bdbb70372c7526b86d71f9769203d96600d1dd7
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2025-05-17 09:38:57 -0700

    .

--- tests/analysis/pointer.zig
+++ tests/analysis/pointer.zig
@@ -38,23 +38,29 @@ const one_u32_orelse = one_u32 orelse unreachable;
 const one_u32_unwrap = one_u32.?;
 //    ^^^^^^^^^^^^^^ (unknown)()
 
+const one_plus_u8 = one_u32 + runtime_u8;
+//    ^^^^^^^^^^^ (unknown)()
+
+const one_plus_i8 = one_u32 + runtime_i8;
+//    ^^^^^^^^^^^ (unknown)()
+
+const one_minus_u8 = one_u32 - runtime_u8;
+//    ^^^^^^^^^^^^ (unknown)()
+
+const one_minus_i8 = one_u32 - runtime_i8;
+//    ^^^^^^^^^^^^ (unknown)()
+
 const one_minus_one = one_u32 - one_u32;
 //    ^^^^^^^^^^^^^ (usize)()
 
 const one_minus_many = one_u32 - many_u32;
 //    ^^^^^^^^^^^^^^ (usize)()
 
-const one_minus_c = one_u32 - c_u32;
-//    ^^^^^^^^^^^ (usize)()
-
-const one_plus_u8 = one_u32 + runtime_u8;
-//    ^^^^^^^^^^^ (unknown)()
-
-const one_minus_u8 = one_u32 - runtime_u8;
-//    ^^^^^^^^^^^^ (unknown)()
-
 const one_minus_slice = one_u32 - slice_u32;
 //    ^^^^^^^^^^^^^^^ (unknown)()
+
+const one_minus_c = one_u32 - c_u32;
+//    ^^^^^^^^^^^ (usize)()
 
 //
 // many item pointer [*]T
@@ -87,7 +93,15 @@ const many_u32_unwrap = many_u32.?;
 const many_plus_u8 = many_u32 + runtime_u8;
 //    ^^^^^^^^^^^^ ([*]const u32)()
 
+// TODO this should be `unknown`
+const many_plus_i8 = many_u32 + runtime_i8;
+//    ^^^^^^^^^^^^ ([*]const u32)()
+
 const many_minus_u8 = many_u32 - runtime_u8;
+//    ^^^^^^^^^^^^^ ([*]const u32)()
+
+// TODO this should be `unknown`
+const many_minus_i8 = many_u32 - runtime_i8;
 //    ^^^^^^^^^^^^^ ([*]const u32)()
 
 const many_minus_one = many_u32 - one_u32;
@@ -96,19 +110,11 @@ const many_minus_one = many_u32 - one_u32;
 const many_minus_many = many_u32 - many_u32;
 //    ^^^^^^^^^^^^^^^ (usize)()
 
-const many_minus_c = many_u32 - c_u32;
-//    ^^^^^^^^^^^^ (usize)()
-
-// TODO this should be `unknown`
-const many_plus_i8 = many_u32 + runtime_i8;
-//    ^^^^^^^^^^^^ ([*]const u32)()
-
-// TODO this should be `unknown`
-const many_minus_i8 = many_u32 - runtime_i8;
-//    ^^^^^^^^^^^^^ ([*]const u32)()
-
 const many_minus_slice = many_u32 - slice_u32;
 //    ^^^^^^^^^^^^^^^^ (unknown)()
+
+const many_minus_c = many_u32 - c_u32;
+//    ^^^^^^^^^^^^ (usize)()
 
 //
 // slice []T
@@ -142,7 +148,13 @@ const slice_u32_unwrap = slice_u32.?;
 const slice_plus_u8 = slice_u32 + runtime_u8;
 //    ^^^^^^^^^^^^^ (unknown)()
 
+const slice_plus_i8 = slice_u32 + runtime_i8;
+//    ^^^^^^^^^^^^^ (unknown)()
+
 const slice_minus_u8 = slice_u32 - runtime_u8;
+//    ^^^^^^^^^^^^^^ (unknown)()
+
+const slice_minus_i8 = slice_u32 - runtime_i8;
 //    ^^^^^^^^^^^^^^ (unknown)()
 
 const slice_minus_one = slice_u32 - one_u32;
@@ -188,7 +200,15 @@ const c_u32_unwrap = c_u32.?;
 const c_plus_u8 = c_u32 + runtime_u8;
 //    ^^^^^^^^^ ([*c]const u32)()
 
+// TODO this should be `unknown`
+const c_plus_i8 = c_u32 + runtime_i8;
+//    ^^^^^^^^^ ([*c]const u32)()
+
 const c_minus_u8 = c_u32 - runtime_u8;
+//    ^^^^^^^^^^ ([*c]const u32)()
+
+// TODO this should be `unknown`
+const c_minus_i8 = c_u32 - runtime_i8;
 //    ^^^^^^^^^^ ([*c]const u32)()
 
 const c_minus_one = c_u32 - one_u32;
@@ -197,19 +217,11 @@ const c_minus_one = c_u32 - one_u32;
 const c_minus_many = c_u32 - many_u32;
 //    ^^^^^^^^^^^^ (usize)()
 
-const c_minus_c = c_u32 - c_u32;
-//    ^^^^^^^^^ (usize)()
-
-// TODO this should be `unknown`
-const c_plus_i8 = c_u32 + runtime_i8;
-//    ^^^^^^^^^ ([*c]const u32)()
-
-// TODO this should be `unknown`
-const c_minus_i8 = c_u32 - runtime_i8;
-//    ^^^^^^^^^^ ([*c]const u32)()
-
 const c_minus_slice = c_u32 - slice_u32;
 //    ^^^^^^^^^^^^^ (unknown)()
+
+const c_minus_c = c_u32 - c_u32;
+//    ^^^^^^^^^ (usize)()
 
 var runtime_index: usize = 5;
 var runtime_u8: u8 = 1;
