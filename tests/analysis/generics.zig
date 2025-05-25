37816453f2e6d356589763a63a422e8f12be3e12
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2025-05-25 15:34:26 -0700

    Update todo's

--- tests/analysis/generics.zig
+++ tests/analysis/generics.zig
@@ -17,21 +17,21 @@ fn Foo(T: type) type {
 const foo = Foo(u8){};
 //    ^^^ (Foo(u8))()
 
-// TODO this should be `fn (type, ?u8, anytype) void`
+// TODO this should be `fn (U: type, ?u8, ?U) void`
 const bar_fn = Foo(u8).bar;
 //    ^^^^^^ (fn (type, ?u8, ?U) void)()
 
 const bar_call = Foo(u8).bar(i32, null, null);
 //    ^^^^^^^^ (void)()
 
-// TODO this should be `fn (type, i32, anytype) i32`
+// TODO this should be `fn (U: type, i32, U) i32`
 const baz_fn = Foo(i32).baz;
 //    ^^^^^^ (fn (type, i32, U) i32)()
 
 const baz_call = Foo(i32).baz(u8, -42, 42);
 //    ^^^^^^^^ (i32)()
 
-// TODO this should be `fn (type, u8, anytype) anytype`
+// TODO this should be `fn (U: type, u8, U) anytype`
 const qux_fn = Foo(u8).qux;
 //    ^^^^^^ (fn (type, u8, U) u8)()
 
@@ -43,7 +43,7 @@ fn fizz(T: type) ?fn () error{}!struct { ??T } {
     return null;
 }
 
-// TODO this should be `fn (type) anytype`
+// TODO this should be `fn (T: type) ?fn () error{}!struct { ??T })()`
 const fizz_fn = fizz;
 //    ^^^^^^^ (fn (type) ?fn () error{}!struct { ??T })()
 
