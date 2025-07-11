533c9b1dc76589c416aa8a6e98cc8a97f6f83582
Techatrix <techatrix@mailbox.org>
2025-07-11 09:06:04 +0200

    nix: update to nixos-25.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     zig-overlay.url = "github:mitchellh/zig-overlay";
     zig-overlay.inputs.nixpkgs.follows = "nixpkgs";
