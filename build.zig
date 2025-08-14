2e181b3605ce91f7b3817120ace08d1c83404983
Techatrix <techatrix@mailbox.org>
2025-08-14 20:18:51 +0200

    set minimum zig version to `0.15.0-dev.1526+b8124d9c0`

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ const builtin = @import("builtin");
 const zls_version: std.SemanticVersion = .{ .major = 0, .minor = 15, .patch = 0, .pre = "dev" };
 
 /// Specify the minimum Zig version that is required to compile and test ZLS:
-/// remove RingBuffer; remove BoundedArray; use @memmove
+/// std.io.Writer.Allocating: rename getWritten() to written()
 ///
 /// If you do not use Nix, a ZLS maintainer can take care of this.
 /// Whenever this version is increased, run the following command:
@@ -15,7 +15,7 @@ const zls_version: std.SemanticVersion = .{ .major = 0, .minor = 15, .patch = 0,
 /// ```
 ///
 /// Also update the `minimum_zig_version` in `build.zig.zon`.
-const minimum_build_zig_version = "0.15.0-dev.1425+7ee6dab39";
+const minimum_build_zig_version = "0.15.0-dev.1526+b8124d9c0";
 
 /// Specify the minimum Zig version that is usable with ZLS:
 /// std.Io.poll: update to new I/O API
