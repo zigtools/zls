c165232b5d6418af1e655f5ef759b4e95ea5dbae
Johan Persson <self@jzp.se>
2026-01-19 12:45:40 +0100

    Fix removed refAllDeclsRecursive call
    The commit https://codeberg.org/ziglang/zig/commit/790d28d6cd9029805af857d56645a6ce191bfe91
    removed `refAllDeclsRecursive` in favor of `refAllDecls`.
    
    Temporarily depends on the following commits to `lsp-kit` and `known-folders`:
    * https://github.com/jozip/known-folders/commit/2f6257e5e6a791d8750c3a4dc84464f045ccb275
    * https://github.com/jozip/lsp-kit/commit/2096fa92f97c3f9df4be9ec1dc101aa5b3f145c3

--- src/analyser/analyser.zig
+++ src/analyser/analyser.zig
@@ -1,9 +1,13 @@
 pub const completions = @import("completions.zig");
 pub const InternPool = @import("InternPool.zig");
-pub const StringPool = @import("string_pool.zig").StringPool;
+pub const string_pool = @import("string_pool.zig");
+pub const StringPool = string_pool.StringPool;
 pub const degibberish = @import("degibberish.zig");
 
 comptime {
     const std = @import("std");
-    std.testing.refAllDeclsRecursive(@This());
+    std.testing.refAllDecls(completions);
+    std.testing.refAllDecls(InternPool);
+    std.testing.refAllDecls(string_pool);
+    std.testing.refAllDecls(degibberish);
 }
