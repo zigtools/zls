4e63f5158bac0bae44db07f525c6b2a04f4a3b84
Techatrix <techatrix@mailbox.org>
2026-02-09 18:14:41 +0100

    set minimum runtime Zig version to `0.16.0-dev.2535+b5bd49460`

--- build.zig
+++ build.zig
@@ -6,10 +6,10 @@ const zls_version = std.SemanticVersion.parse(@import("build.zig.zon").version) 
 const minimum_build_zig_version = @import("build.zig.zon").minimum_zig_version;
 
 /// Specify the minimum Zig version that is usable with ZLS:
-/// std.Thread sync primitives roundup
+/// std.Threaded: replace more kernel32 functions with ntdll
 ///
 /// A breaking change to the Zig Build System should be handled by updating ZLS's build runner (see src\build_runner)
-const minimum_runtime_zig_version = "0.16.0-dev.2475+e7e700334";
+const minimum_runtime_zig_version = "0.16.0-dev.2535+b5bd49460";
 
 const release_targets = [_]std.Target.Query{
     .{ .cpu_arch = .aarch64, .os_tag = .linux },
