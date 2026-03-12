fb9843a56f389f52cdc309f91709b7836e781402
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2026-03-11 19:57:23 -0700

    Fix resolved type of type function with unknown return type

--- tests/analysis/function.zig
+++ tests/analysis/function.zig
@@ -2,3 +2,11 @@ fn func() addrspace(.generic) linksection(.{}) callconv(.auto) void {}
 //                  ^^^^^^^^ (AddressSpace)()
 //                                        ^ ([]const u8)()
 //                                                      ^^^^^ (void)()
+
+// zig fmt: off
+fn Unknown() type { return undefined.Unknown; }
+// ^^^^^^^ (fn () type)()
+// zig fmt: on
+
+const UnknownCall = Unknown();
+//    ^^^^^^^^^^^ (type)((unknown type))
