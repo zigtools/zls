a856fcf4038a257983b3129609232db84f7f7c98
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2026-03-12 21:58:32 -0700

    Avoid using interned value when resolving variable

--- tests/analysis/variable.zig
+++ tests/analysis/variable.zig
@@ -1,3 +1,6 @@
 const variable addrspace(.generic) linksection(.{}) = 0;
 //                       ^^^^^^^^ (AddressSpace)()
 //                                             ^ ([]const u8)()
+
+var bool_var = true;
+//  ^^^^^^^^ (bool)((unknown value))
